e1bf4ab79c85b256bbe576c4e719e9fd85d0e5d5
Andrew Dass <dassandrew3@gmail.com>
2026-07-28 15:24:26 -0400

    daily exercises for week 4, still have to do two more problems

--- src/exercises/lesson-04/BugEventPropagation.jsx
+++ src/exercises/lesson-04/BugEventPropagation.jsx
@@ -1,13 +1,17 @@
 // TOPIC: Event Bubbling & Stopping Propagation
 // TASK: Ensure only the inner button's action triggers an alert when the button is pushed
 
+import { useRef } from 'react';
+
 export default function BugEventPropagation() {
+  let ref = useRef(0);
+
   function handleOuterClick() {
-    alert("RED BOX CLICKED ❌ Don't show me!");
+    ref.current = alert("RED BOX CLICKED ❌ Don't show me!");
   }
 
   function handleInnerClick() {
-    alert('Button Clicked ✅');
+    ref.current = alert('Button Clicked ✅');
   }
 
   return (
@@ -17,8 +21,20 @@ export default function BugEventPropagation() {
         style={{ padding: 20, border: '2px solid red' }}
         onClick={handleOuterClick}
       >
-        <button onClick={handleInnerClick}>Click inner button</button>
+        <button
+          onClick={handleInnerClick}
+          style={{ padding: 20, border: '2px solid blue' }}
+        >
+          Click inner button
+        </button>
       </div>
     </>
   );
 }
+
+// Did not solve, not really sure how to
+
+// Tried putting border around button itself though it
+// changes the display and there aren't two objects now
+// two objects meaning border and button, it would just
+// be a button with a different shading
